5ac9d43b772cb131338d4f16af897bff9cba963e
Terry Smith <terry@icedteapowered.com>
2020-10-18 12:33:22 -0400

    Add server code.

---
+++ Source/Server/main.cpp
@@ -0,0 +1,142 @@
+
+#include <Core/MetaSystem.h>
+#include <Core/Application.h>
+#include <Core/World.h>
+#include <Meta/MetaData.h>
+#include <IO/ResourceSystem.h>
+#include <Core/ErrorSystem.h>
+#include <Core/TimeSystem.h>
+#include <Render/RenderSystem.h>
+#include <IO/Window.h>
+#include <IO/InputSystem.h>
+#include <Core/MessageSystem.h>
+#include <Core/Timer.h>
+#include <Render/MaterialSystem.h>
+#include <Scripting/ScriptingSystem.h>
+#include <Network/NetworkSystem.h>
+#include <Audio/AudioSystem.h>
+#include <Render/AnimationSystem.h>
+#include <Render/TerrainSystem.h>
+#include <IO/LogSystem.h>
+#include <IO/Profiler.h>
+#include <IO/SQLiteDataLoader.h>
+#include <Core/TransformComponent.h>
+
+int main(int argc, const char* argv[]) {
+    // Get application instance
+    Application* app = Application::GetInstance();
+
+    // Create systems
+    MetaSystem* metaSystem = app->CreateSystem<MetaSystem>();
+    ResourceSystem* resourceSystem = app->CreateSystem<ResourceSystem>();
+    ErrorSystem* errorSystem = app->CreateSystem<ErrorSystem>();
+    TerrainSystem* terrainSystem = app->CreateSystem<TerrainSystem>();
+    RenderSystem* renderSystem = app->CreateSystem<RenderSystem>();
+    MessageSystem* messageSystem = app->CreateSystem<MessageSystem>();
+    TimeSystem* timeSystem = app->CreateSystem<TimeSystem>();
+    InputSystem* inputSystem = app->CreateSystem<InputSystem>();
+    MaterialSystem* materialSystem = app->CreateSystem<MaterialSystem>();
+    ScriptingSystem* scriptingSystem = app->CreateSystem<ScriptingSystem>();
+    NetworkSystem* networkSystem = app->CreateSystem<NetworkSystem>();
+    AudioSystem* audioSystem = app->CreateSystem<AudioSystem>();
+    AnimationSystem* animationSystem = app->CreateSystem<AnimationSystem>();
+    LogSystem* logSystem = app->CreateSystem<LogSystem>();
+
+    // Initialize systems
+    app->Initialize();
+
+    // Open error log
+    logSystem->Open("error.log");
+
+    // Set up resource paths
+    resourceSystem->AddSearchPath("Resources/Shaders");
+    resourceSystem->AddSearchPath("Resources/Models");
+    resourceSystem->AddSearchPath("Resources/Textures");
+    resourceSystem->AddSearchPath("Resources/Terrain");
+    resourceSystem->AddSearchPath("Resources/Scripts");
+    resourceSystem->AddSearchPath("Resources/Audio");
+
+    // Register meta data
+    MetaData::RegisterMetaFunctions();
+
+    // Initialize the world
+    World* world = World::GetInstance();
+
+    // Start scripting system
+    scriptingSystem->Start();
+
+    // Register built-in component types
+    metaSystem->RegisterComponentType("TransformComponent");
+    metaSystem->RegisterComponentType("AudioComponent");
+    metaSystem->RegisterComponentType("RigidBodyComponent");
+    metaSystem->RegisterComponentType("CameraComponent");
+    metaSystem->RegisterComponentType("DirectionalLightComponent");
+    metaSystem->RegisterComponentType("MeshComponent");
+    metaSystem->RegisterComponentType("PointLightComponent");
+    metaSystem->RegisterComponentType("Skybox");
+    metaSystem->RegisterComponentType("TerrainComponent");
+    metaSystem->RegisterComponentType("ScriptComponent");
+    metaSystem->RegisterComponentType("BillboardComponent");
+    metaSystem->RegisterComponentType("ParticleEmitterComponent");
+
+    // Load game code
+    if (Resource::Exists("game.dll")) {
+        scriptingSystem->LoadLibrary("game.dll");
+    }
+
+    // Open our game database
+    SQLiteDataLoader* dataLoader = new SQLiteDataLoader();
+    dataLoader->Open("game.db");
+
+    // Load entities
+    Array<GigaObject*> entities = dataLoader->GetObjects("Entity");
+
+    // Add entities to world
+    auto it = entities.begin();
+    for (; it != entities.end(); it++) {
+        Entity* entity = dynamic_cast<Entity*>(*it);
+        world->AddEntity(entity);
+    }
+
+    // Load components
+    Array<std::string> componentTypes = metaSystem->GetComponentTypes();
+    auto cti = componentTypes.begin();
+    for (; cti != componentTypes.end(); cti++) {
+        Array<GigaObject*> components = dataLoader->GetObjects(*cti);
+        auto ci = components.begin();
+        for (; ci != components.end(); ci++) {
+            // Check whether this is attached to an entity
+            Component* component = dynamic_cast<Component*>(*ci);
+            Entity* entity = component->GetParent();
+            if (entity != 0) {
+
+            }
+        }
+    }
+
+    // Start server
+    scriptingSystem->CallFunction("Pokeclone", "StartServer", 0, 0);
+
+     // Game timer
+    Timer* gameTimer = new Timer();
+    gameTimer->Start();
+
+    // Main loop
+    while (true) {
+        PROFILE_START_FRAME();
+
+        float delta = gameTimer->Duration();
+        gameTimer->Reset();
+
+        app->Update(delta);
+
+        PROFILE_END_FRAME();
+        timeSystem->Sleep(1);
+    }
+
+    // Close log
+    Profiler::GetInstance()->Log();
+    logSystem->Close();
+
+    return 0;
+}
