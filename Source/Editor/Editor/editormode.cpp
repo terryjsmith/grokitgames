29de5ad92c7c5dcc7b64b84ca0a5f85ba8fcc85f
Terry Smith <terry@icedteapowered.com>
2021-03-10 20:11:40 -0500

    Remove previous editor version

--- Source/Editor/Editor/editormode.cpp
+++
@@ -1,7 +0,0 @@
-#include "editormode.h"
-
-EditorMode::EditorMode()
-{
-    m_selectedEntity = 0;
-    m_selectedMesh = 0;
-}
--- Source/Editor/Editor/gizmocomponent.cpp
+++
@@ -1,6 +0,0 @@
-#include "gizmocomponent.h"
-
-GizmoComponent::GizmoComponent()
-{
-
-}
